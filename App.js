fc915ec318d7e1207a38f5b1ecd4ad1c84f55bf6
medal17 <iyandaadeshina@gmail.com>
2021-10-22 17:48:50 +0100

    adds map to app

--- App.js
+++ App.js
@@ -10,6 +10,11 @@ import LoginScreen from './screens/LoginScreen';
 import SignupScreen from './screens/SignupScreen';
 import HomeScreen from './screens/HomeScreen';
 import ShowItemDetails from './screens/ShowItemDetails';
+import DispatchScreen from './screens/DispatchScreen';
+import ShopsScreen from './screens/ShopsScreen';
+import OrdersScreen from './screens/OrdersScreen';
+import RidersScreen from './screens/RidersScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 
 const Stack = createNativeStackNavigator();
@@ -29,6 +34,11 @@ export default function App() {
         <Stack.Screen name='signup' component={SignupScreen}/>
         <Stack.Screen name='homeScreen' component={HomeScreen}/>
         <Stack.Screen name='showItemDetails' component={ShowItemDetails}/>
+        <Stack.Screen name='dispatchScreen' component={DispatchScreen}/>
+        <Stack.Screen name='storeScreen' component={ShopsScreen}/>
+        <Stack.Screen name='OrdersScreen' component={OrdersScreen}/>
+        <Stack.Screen name='RidersScreen' component={RidersScreen}/>
+        <Stack.Screen name='ProfileScreen' component={ProfileScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ screens/OrdersScreen.js
@@ -0,0 +1,49 @@
+// import { CurrentRenderContext } from '@react-navigation/core'
+// import { NavigationContainer } from '@react-navigation/native';
+import React from 'react'
+import { StyleSheet, Text, KeyboardAvoidingView, TextInput, Image, View, Button, TouchableOpacity, Platform } from 'react-native'
+import tw from 'tailwind-react-native-classnames';
+
+const OrdersScreen = ({navigation}) => {
+    return (
+        <KeyboardAvoidingView style={tw`py-20`} behavior={Platform.OS=='ios'?'padding':'height'}>
+            <View style={tw`bg-purple-100 my-auto mx-5 rounded-xl h-full`}>
+                <Image
+                    source={require('../assets/bashMed.png')}
+                    style={tw`mx-auto mt-10`}
+                />
+                <TextInput
+                    placeholder='Username'
+                    style={tw`mt-10 py-2 bg-gray-100 mx-5 border-b-2 border-purple-400 pl-2`}
+                />
+                 <TextInput
+                    placeholder='Password'
+                    style={tw`mt-3 py-2 bg-gray-100 mx-5 border-b-2 border-purple-400 pl-2`}
+                />
+                <TouchableOpacity onPress={()=>navigation.navigate('homeScreen')} style={tw`mx-5 my-5 bg-purple-900 rounded-full py-3`}>
+                   <Text style={tw`m-auto text-white text-sm`}>Login</Text>
+                </TouchableOpacity>
+                 <TouchableOpacity onPress={()=>navigation.navigate('signup')}>
+                   <Text style={tw`m-auto text-white text-base text-purple-900`}>Sign up instead</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
+    )
+}
+
+export default OrdersScreen
+
+const styles = StyleSheet.create({
+    formContainer:{
+        backgroundColor:'#8B7DF923',
+        margin: 20,
+        height: '80%',
+        width:'90%',
+        justifyContent: 'center',
+        // alignSelf: 'center',
+        alignContent: 'center',
+        justifyContent:'flex-start',
+        padding:20,
+        borderRadius:10
+    }
+})
